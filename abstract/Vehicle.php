74dcef334b69f36df1d428de010cf655fb306770
LidaMudota <ktopro4italtotzdohnet@yandex.ru>
2025-07-26 16:21:24 +0300

    update vehicle

--- abstract/Vehicle.php
+++ abstract/Vehicle.php
@@ -1,5 +1,7 @@
 <?php
-abstract class Vehicle {
+require_once __DIR__ . '/../interfaces/VehicleInterface.php';
+
+abstract class Vehicle implements VehicleInterface {
     protected string $interior = 'Простой интерьер';
 
     public function setInterior(string $interior): void {
@@ -7,13 +9,18 @@ abstract class Vehicle {
     }
 
     public function honk(): void {
-    echo "Сигнал!" . PHP_EOL;
+        echo 'Сигнал!' . PHP_EOL;
     }
 
     public function wipersOn(): void {
-    echo "Дворники включены" . PHP_EOL;
+        echo 'Дворники включены' . PHP_EOL;
     }
+
     public function showInterior(): void {
         echo 'Интерьер: ' . $this->interior . PHP_EOL;
     }
+
+    abstract public function moveForward();
+    abstract public function moveBackward();
+    abstract public function useAbility();
 }
